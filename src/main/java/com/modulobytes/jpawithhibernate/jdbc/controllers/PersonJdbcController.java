6aa643c89175c4eb5193dc123cd8d3526c9ea9a7
nitesh chaturvedi <nitesh@modulobytes.com>
2024-02-21 18:57:55 +0530

    removing starter web dependency to make it a command line app

--- src/main/java/com/modulobytes/jpawithhibernate/jdbc/controllers/PersonJdbcController.java
+++ src/main/java/com/modulobytes/jpawithhibernate/jdbc/controllers/PersonJdbcController.java
@@ -2,14 +2,9 @@ package com.modulobytes.jpawithhibernate.jdbc.controllers;
 
 import com.modulobytes.jpawithhibernate.jdbc.beans.Person;
 import com.modulobytes.jpawithhibernate.jdbc.repositories.PersonJdbcDao;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "/person")
 public class PersonJdbcController {
 
     private final PersonJdbcDao personJdbcDao;
@@ -18,7 +13,6 @@ public class PersonJdbcController {
         this.personJdbcDao = personJdbcDao;
     }
 
-    @GetMapping(path = "/all")
     public List<Person> findAll() {
         return personJdbcDao.findAll();
     }
